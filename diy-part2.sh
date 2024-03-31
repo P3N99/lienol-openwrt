6bdca4bb7085993163829ce042195a4f3f32810b
P3N99 <145282517+P3N99@users.noreply.github.com>
2024-04-01 02:56:38 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,13 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把MSG1500修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i 's/OpenWrt/CR660X/g' package/base-files/files/bin/config_generate
+
+# Add luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
